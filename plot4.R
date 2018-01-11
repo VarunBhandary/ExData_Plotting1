1354c5ac53f8ad0320901f58b14f695dc414ce4e
DataErrata <varun.bhandary@gmail.com>
2018-01-11 14:11:15 +0000

    Updating Plot 4 R Code and Output with Correct Y Label

--- plot4.R
+++ plot4.R
@@ -64,7 +64,7 @@ par(mfrow = c(2,2))
 plot(data$DateTime, data$Global_active_power, type="l", ylab="Global Active Power", xlab="")
 
 ##Plot 2
-plot(data$DateTime, data$Voltage, type="l", ylab="Global Active Power", xlab="datetime")
+plot(data$DateTime, data$Voltage, type="l", ylab="Voltage (Volt)", xlab="datetime")
 
 ##Plot 3
 plot(data$DateTime, data$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
@@ -72,8 +72,8 @@ lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
 lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 
-##Plot 2
-plot(data$DateTime, data$Global_reactive_power, type="l", ylab="Global Active Power", xlab="datetime")
+##Plot 4
+plot(data$DateTime, data$Global_reactive_power, type="l", ylab="Global Reactive Power", xlab="datetime")
 
 
 # Closing the file connection
